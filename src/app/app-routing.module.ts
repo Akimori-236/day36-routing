4a9cb509f01b6180d8d98dff943888e9c6d8920d
Wee Seng <wee.bscl@gmail.com>
2023-03-28 13:23:16 +0800

    EOD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home.component';
-import { CatComponent } from './components/cat.component';
-import { DogComponent } from './components/dog.component';
-import { CustomerComponent } from './components/customer.component';
+import { HomeComponent } from './components/routing/home.component';
+import { CatComponent } from './components/routing/cat.component';
+import { DogComponent } from './components/routing/dog.component';
+import { CustomerComponent } from './components/routing/customer.component';
+import { ProductComponent } from './components/nestedRouting/product.component';
+import { ProductDetailComponent } from './components/nestedRouting/product-detail.component';
+import { ProductInfoComponent } from './components/nestedRouting/product-info.component';
+import { ProductSpecsComponent } from './components/nestedRouting/product-specs.component';
 
+// ROUTING
+// const routes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'cat', component: CatComponent },
+//   { path: 'dog', component: DogComponent },
+//   { path: 'customer/:custId', component: CustomerComponent },
+//   { path: '**', redirectTo: '/', pathMatch: 'full' },
+// ];
+
+// NESTED ROUTES
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'cat', component: CatComponent },
-  { path: 'dog', component: DogComponent },
-  { path: 'customer/:custId', component: CustomerComponent },
-  { path: '**', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: ProductComponent },
+  {
+    path: 'product', component: ProductComponent,
+    children: [
+      {
+        path: 'details', component: ProductDetailComponent,
+        children: [
+          { path: 'info', component: ProductInfoComponent },
+          { path: 'specs', component: ProductSpecsComponent },
+          { path: '**', redirectTo: 'details', pathMatch: "full" }
+        ]
+      },
+    ]
+  },
+  {path: '**', redirectTo: '/', pathMatch:"full"}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home.component';
-import { CatComponent } from './components/cat.component';
-import { DogComponent } from './components/dog.component';
-import { CustomerComponent } from './components/customer.component';
+import { HomeComponent } from './components/routing/home.component';
+import { CatComponent } from './components/routing/cat.component';
+import { DogComponent } from './components/routing/dog.component';
+import { CustomerComponent } from './components/routing/customer.component';
+import { ProductComponent } from './components/nestedRouting/product.component';
+import { ProductDetailComponent } from './components/nestedRouting/product-detail.component';
+import { ProductInfoComponent } from './components/nestedRouting/product-info.component';
+import { ProductSpecsComponent } from './components/nestedRouting/product-specs.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { CustomerComponent } from './components/customer.component';
     HomeComponent,
     CatComponent,
     DogComponent,
-    CustomerComponent
+    CustomerComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ProductInfoComponent,
+    ProductSpecsComponent
   ],
   imports: [
     BrowserModule,
